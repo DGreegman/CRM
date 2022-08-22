b7347a04b2a1f9ba3447d2d6db62e2917f1b5934
DGreegman <obeagug@gmail.com>
2022-08-22 08:05:45 -0600

    Structured my template and worked on the views

--- crms/views.py
+++ crms/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+def home(request):
+    customers = Customer.objects.all()
+    orders = Order.objects.all()
+
+    total_customers = customers.count()
+    total_order = orders.count()
+    pending = orders.filter(status='Pending').count()
+    delivered = orders.filter(status='Delivered').count()
+    context = {
+        'customers': customers,
+        'orders' : orders, 
+        'total_customers' : total_customers,
+        'total_order': total_order,
+        'delivered': delivered,
+        'pending': pending
+    }
+    return render(request, 'crms/dashboard.html', context)
+
+
+def products(request):
+    products = Product.objects.all()
+    return render(request, 'crms/products.html', {'products':products})
+
+
+def customer(request):
+    return render(request, 'crms/customer.html')
